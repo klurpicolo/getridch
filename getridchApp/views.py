881d2c687fc7bc75de8f35beda0e14279f6a9427
WELLYTP\Welly-TP <well@test.dev>
2018-12-01 11:08:50 +0700

    Well 2nd Commit - Line Bot Response

--- getridchApp/views.py
+++ getridchApp/views.py
@@ -6,12 +6,14 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-line_bot_api = LineBotApi('FM6Q6U/BAWJlaQMzPOneNK6QT0Di9qZ93VtKt4w6NnnX1U6/1fqduzsoAjW3/kpnVrLj4GGobh/XsVqZaccp+Ra44QRKxrL6m8fFFDhUOpGvDKFu5P4Qf1c1I0qe6p9FAA7Mno9YPr510Sfh3B8xxwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser('1e7ab9437dc85f54d08cf117425398ca')
 
 
 @csrf_exempt
 def callback(request):
+    if request.method == 'GET':
+        return HttpResponse(status=200)
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
@@ -26,11 +28,8 @@ def callback(request):
         for event in events:
             if isinstance(event, MessageEvent):
                 if isinstance(event.message, TextMessage):
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text=event.message.text)
-                    )
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
-        return HttpResponse()
+        return HttpResponse(status=200)
     else:
-        return HttpResponseBadRequest()
+        return HttpResponseBadRequest(status=400)
