0cc2231d8e11489584607915613476f6422df5ff
WELLYTP\Welly-TP <well@test.dev>
2018-12-01 20:55:10 +0700

    test

--- getridchApp/views.py
+++ getridchApp/views.py
@@ -157,6 +157,20 @@ def handle_text_message(event):
                                                                    MessageAction(label='No', text='No!')])
         template_message = TemplateSendMessage(alt_text='Confirm alt text', template=confirm_template)
         line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'list':
+        carousel_template = CarouselTemplate(columns=[
+            CarouselColumn(text='hoge1', title='fuga1', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping')
+            ]),
+            CarouselColumn(text='hoge2', title='fuga2', actions=[
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米')
+            ]),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Carousel alt text', template=carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
